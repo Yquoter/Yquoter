6774b2c0f212404a888f36bb44430bc5f0f81f11
Yodeesy <646726564@qq.com>
2025-10-28 22:28:30 +0800

    tagging

--- src/yquoter/__init__.py
+++ src/yquoter/__init__.py
@@ -19,6 +19,7 @@ import logging
 from yquoter.logger import setup_logging, get_logger
 setup_logging(level=logging.WARNING)
 logger = get_logger(__name__)
+
 # ----------------------------------------------------------------------
 # Core imports
 # ----------------------------------------------------------------------
